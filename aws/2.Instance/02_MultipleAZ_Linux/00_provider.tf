7e7f3402bf54e63c26e36be61ec567da386bf2b8
Praparn Lungpoonlap <eva10409@gmail.com>
2022-05-14 15:39:52 +0700

    update provider

--- aws/2.Instance/02_MultipleAZ_Linux/00_provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-    access_key = var.access_key // Set environment variable on your local -->  TF_VAR_access_key="YOUR_ACCESS_KEY"
-    secret_key = var.secret_key // Set environment variable on your local -->  TF_VAR_secret_key="YOUR_SECRET_KEY"
-    region = var.region
-}
